b0df009d03cc5571110d579e9ba3a7c970a1ebd9
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-09-20 23:46:15 -0300

    feat: adiciona busca por email

--- src/main/java/br/com/gustavobarbosa/hidrat_ai/repositories/EstabelecimentoRepository.java
+++ src/main/java/br/com/gustavobarbosa/hidrat_ai/repositories/EstabelecimentoRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, UUID> {
     List<Estabelecimento> findAllByAtivoTrue();
+
+    Optional<Estabelecimento> findByEmail(String email);
 }
