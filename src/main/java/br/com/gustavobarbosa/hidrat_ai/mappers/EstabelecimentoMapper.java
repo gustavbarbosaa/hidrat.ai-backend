76b2ce7e6eaef7205937f80ac123c010a8e9d2e2
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-09-20 22:58:48 -0300

    feat: cria mapper para converter entidades e dtos

---
+++ src/main/java/br/com/gustavobarbosa/hidrat_ai/mappers/EstabelecimentoMapper.java
@@ -0,0 +1,27 @@
+package br.com.gustavobarbosa.hidrat_ai.mappers;
+
+import br.com.gustavobarbosa.hidrat_ai.domain.Estabelecimento;
+import br.com.gustavobarbosa.hidrat_ai.dto.request.EstabelecimentoRequest;
+import br.com.gustavobarbosa.hidrat_ai.dto.response.EstabelecimentoResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EstabelecimentoMapper {
+    public EstabelecimentoResponse paraResponse(Estabelecimento estabelecimento) {
+        return new EstabelecimentoResponse(
+                estabelecimento.getId(),
+                estabelecimento.getNome(),
+                estabelecimento.getApelido()
+        );
+    }
+
+    public Estabelecimento paraEntidade(EstabelecimentoRequest estabelecimentoRequest) {
+        return Estabelecimento.builder()
+                .nome(estabelecimentoRequest.nome().trim())
+                .apelido(estabelecimentoRequest.apelido().trim())
+                .email(estabelecimentoRequest.email().trim().toLowerCase())
+                .senha(estabelecimentoRequest.senha())
+                .cpfCnpj(estabelecimentoRequest.cpfCnpj().replaceAll("\\D", ""))
+                .build();
+    }
+}
