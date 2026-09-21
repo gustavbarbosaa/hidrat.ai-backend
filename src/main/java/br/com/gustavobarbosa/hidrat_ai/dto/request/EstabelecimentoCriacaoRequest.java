a7a0f825f9f94524138ca16482dc67c536bfb44e
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-09-21 19:18:38 -0300

    chore: separa casos de request para edição e cadastro

---
+++ src/main/java/br/com/gustavobarbosa/hidrat_ai/dto/request/EstabelecimentoCriacaoRequest.java
@@ -0,0 +1,25 @@
+package br.com.gustavobarbosa.hidrat_ai.dto.request;
+
+import br.com.gustavobarbosa.hidrat_ai.validation.CpfOuCnpj;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record EstabelecimentoCriacaoRequest(
+        @NotBlank(message = "O campo nome é obrigatório.")
+        @Size(max = 100, message = "O campo nome deve ter no máximo 100 caracteres.")
+        String nome,
+        @NotBlank(message = "O campo apelido é obrigatório.")
+        @Size(max = 100, message = "O campo apelido deve ter no máximo 100 caracteres.")
+        String apelido,
+        @NotBlank(message = "O campo email é obrigatório.")
+        @Size(max = 100, message = "O campo email deve ter no máximo 100 caracteres.")
+        @Email(message = "O e-mail informado é inválido.")
+        String email,
+        @NotBlank(message = "O campo senha é obrigatório.")
+        @Size(min = 8, max = 100, message = "A senha deve ter entre 8 a 100 caracteres.")
+        String senha,
+        @NotBlank(message = "O campo CPF/CNPJ é obrigatório.")
+        @CpfOuCnpj
+        String cpfCnpj
+) { }
--- src/main/java/br/com/gustavobarbosa/hidrat_ai/mappers/EstabelecimentoMapper.java
+++ src/main/java/br/com/gustavobarbosa/hidrat_ai/mappers/EstabelecimentoMapper.java
@@ -1,7 +1,8 @@
 package br.com.gustavobarbosa.hidrat_ai.mappers;
 
 import br.com.gustavobarbosa.hidrat_ai.domain.Estabelecimento;
-import br.com.gustavobarbosa.hidrat_ai.dto.request.EstabelecimentoRequest;
+import br.com.gustavobarbosa.hidrat_ai.dto.request.EstabelecimentoCriacaoRequest;
+import br.com.gustavobarbosa.hidrat_ai.dto.request.EstabelecimentoEdicaoRequest;
 import br.com.gustavobarbosa.hidrat_ai.dto.response.EstabelecimentoResponse;
 import org.springframework.stereotype.Component;
 
@@ -16,13 +17,22 @@ public class EstabelecimentoMapper {
         );
     }
 
-    public Estabelecimento paraEntidade(EstabelecimentoRequest estabelecimentoRequest) {
+    public Estabelecimento paraEntidade(EstabelecimentoCriacaoRequest estabelecimentoCriacaoRequest) {
         return Estabelecimento.builder()
-                .nome(estabelecimentoRequest.nome().trim())
-                .apelido(estabelecimentoRequest.apelido().trim())
-                .email(estabelecimentoRequest.email().trim().toLowerCase())
-                .senha(estabelecimentoRequest.senha())
-                .cpfCnpj(estabelecimentoRequest.cpfCnpj().replaceAll("\\D", ""))
+                .nome(estabelecimentoCriacaoRequest.nome().trim())
+                .apelido(estabelecimentoCriacaoRequest.apelido().trim())
+                .email(estabelecimentoCriacaoRequest.email().trim().toLowerCase())
+                .senha(estabelecimentoCriacaoRequest.senha())
+                .cpfCnpj(estabelecimentoCriacaoRequest.cpfCnpj().replaceAll("\\D", ""))
+                .build();
+    }
+
+    public Estabelecimento paraEntidade(EstabelecimentoEdicaoRequest estabelecimentoCriacaoRequest) {
+        return Estabelecimento.builder()
+                .nome(estabelecimentoCriacaoRequest.nome().trim())
+                .apelido(estabelecimentoCriacaoRequest.apelido().trim())
+                .email(estabelecimentoCriacaoRequest.email().trim().toLowerCase())
+                .cpfCnpj(estabelecimentoCriacaoRequest.cpfCnpj().replaceAll("\\D", ""))
                 .build();
     }
 }
