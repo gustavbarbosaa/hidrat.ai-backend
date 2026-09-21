e2485d991c1009fe218473a67d5a5c9772af7103
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-09-20 21:56:26 -0300

    Initial commit

---
+++ src/test/java/br/com/gustavobarbosa/hidrat_ai/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavobarbosa.hidrat_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
