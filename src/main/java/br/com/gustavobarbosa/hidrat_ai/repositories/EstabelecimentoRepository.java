dfce476c89867a399553f91d187c8c8dfe95949a
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-09-20 23:21:27 -0300

    feat: adiciona busca por ativos

--- src/main/java/br/com/gustavobarbosa/hidrat_ai/repositories/EstabelecimentoRepository.java
+++ src/main/java/br/com/gustavobarbosa/hidrat_ai/repositories/EstabelecimentoRepository.java
@@ -4,8 +4,10 @@ import br.com.gustavobarbosa.hidrat_ai.domain.Estabelecimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, UUID> {
+    List<Estabelecimento> findAllByAtivoTrue();
 }
