fd52ba818b8dc44fe88c8c85b96ea4fbdeeab2a4
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-09-20 22:57:01 -0300

    feat: cria exception para recursos não encontrados

---
+++ src/main/java/br/com/gustavobarbosa/hidrat_ai/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.gustavobarbosa.hidrat_ai.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
