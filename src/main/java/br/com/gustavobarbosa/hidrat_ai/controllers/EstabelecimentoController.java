6cbb3ed570cb36b21b7f5db782e10908d9e01176
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-09-20 23:21:54 -0300

    feat: adiciona service e controller com funcionalidades de busca por id e todos ativos

---
+++ src/main/java/br/com/gustavobarbosa/hidrat_ai/controllers/EstabelecimentoController.java
@@ -0,0 +1,27 @@
+package br.com.gustavobarbosa.hidrat_ai.controllers;
+
+import br.com.gustavobarbosa.hidrat_ai.dto.response.EstabelecimentoResponse;
+import br.com.gustavobarbosa.hidrat_ai.services.EstabelecimentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/estabelecimentos")
+@RequiredArgsConstructor
+public class EstabelecimentoController {
+    private final EstabelecimentoService estabelecimentoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EstabelecimentoResponse> buscaPorId(@PathVariable UUID id) {
+        return ResponseEntity.ok().body(estabelecimentoService.buscarPorId(id));
+    }
+
+    @GetMapping("ativos")
+    public ResponseEntity<List<EstabelecimentoResponse>> buscarAtivos() {
+        return ResponseEntity.ok().body(estabelecimentoService.buscarAtivos());
+    }
+}
---
+++ src/main/java/br/com/gustavobarbosa/hidrat_ai/services/EstabelecimentoService.java
@@ -0,0 +1,42 @@
+package br.com.gustavobarbosa.hidrat_ai.services;
+
+import br.com.gustavobarbosa.hidrat_ai.domain.Estabelecimento;
+import br.com.gustavobarbosa.hidrat_ai.dto.response.EstabelecimentoResponse;
+import br.com.gustavobarbosa.hidrat_ai.exceptions.RecursoNaoEncontradoException;
+import br.com.gustavobarbosa.hidrat_ai.mappers.EstabelecimentoMapper;
+import br.com.gustavobarbosa.hidrat_ai.repositories.EstabelecimentoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class EstabelecimentoService {
+    private static final String ESTABELECIMENTO_NAO_ENCONTRADO = "Estabelecimento não encontrado.";
+
+    private final EstabelecimentoRepository estabelecimentoRepository;
+    private final EstabelecimentoMapper estabelecimentoMapper;
+
+    public EstabelecimentoResponse buscarPorId(UUID id) {
+        Estabelecimento estabelecimento = buscaEstabelecimento(id);
+
+        return estabelecimentoMapper.paraResponse(estabelecimento);
+    }
+
+    public List<EstabelecimentoResponse> buscarAtivos() {
+        List<Estabelecimento> estabelecimentosAtivos = estabelecimentoRepository.findAllByAtivoTrue();
+
+        return estabelecimentosAtivos
+                .stream()
+                .map(estabelecimentoMapper::paraResponse)
+                .toList();
+    }
+
+    private Estabelecimento buscaEstabelecimento(UUID id) {
+        return estabelecimentoRepository.findById(id)
+                .filter(Estabelecimento::isAtivo)
+                .orElseThrow(() -> new RecursoNaoEncontradoException(ESTABELECIMENTO_NAO_ENCONTRADO));
+    }
+}
